a7db1b6ed1524f434d7b4ead99d9466bd3b6d062
Raj Shrimali <rajshrimali@gmail.com>
2019-05-07 11:59:52 -0400

    removing schema doc in favor of TypeDoc

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,14 @@
 import * as functions from 'firebase-functions';
 
+import express from "express"; 
+
+const app = express(); 
+
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+// build router 
