3f1c84a6a44eb39bb83f3c50bff3cbbaee13dc82
Raj Shrimali <rajshrimali@gmail.com>
2019-05-08 15:23:36 -0400

    prototype file functions, can't test until web interface is done

--- functions/src/app.ts
+++ functions/src/app.ts
@@ -6,6 +6,6 @@
 import * as functions from 'firebase-functions';
 import * as user from "./user" 
 
-module.exports.user =  functions.https.onRequest(user.routedUser); 
+
 module.exports.userCreateAddInfo = functions.https.onCall(user.userCreateAddInfo);  
 module.exports.userDeleteTrigger = functions.auth.user().onDelete(user.onUserDelete); 
--- functions/src/user.ts
+++ functions/src/user.ts
@@ -4,8 +4,6 @@
 
 // import * as admin from 'firebase-admin'; 
 // import * as firebaseHelper from 'firebase-functions-helper'; 
-import * as bodyParser from "body-parser"
-import * as express from "express"; 
 import * as entity from "./entity"; 
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin'; 
@@ -13,48 +11,12 @@ import * as admin from 'firebase-admin';
 admin.initializeApp(functions.config().firebase); 
 const db = admin.firestore(); 
 
-
-
-const userfunc = express(); 
-const app = express.Router(); 
-
-userfunc.use('/', app); 
-userfunc.use(bodyParser.json()); 
-userfunc.use(bodyParser.urlencoded({extended: false})) 
-
-/**
- * Authentication for User Creation - Check Perms Here. 
- */
-app.use((req, res, next) =>{
-    console.log("Checking Perms...."); 
-    //TODO: Check perms. 
-    next(); 
-})
-
-
-/** 
- * Auto Trigger on User Create.  
- * @param req: The request 
- * @param res: The Response object
- */
-app.post('/create', (req, res) => {
-    // Perform validation 
-    // Assume this is only triggered 
-})
-
-/**
- * Quick update of user with user id 
- */
-app.get("/update/:uid", (req, res) => {
-    res.send("Update user with user id.")
-})
-
 /**
  * userCreateAddInfo 
  * context: the firebase auth context 
  * data: the body of the form 
  */
-let userCreateAddInfoFunction = function(data: any, context: any) {
+const userCreateAddInfoFunction = async function(data: any, context: any) {
     // check context 
     if (context.email.slice(-10) !== "gatech.edu") {
         // return error 
@@ -84,22 +46,21 @@ let userCreateAddInfoFunction = function(data: any, context: any) {
             membership_status: entity.MembershipStatus.pending, 
             short_title: ""
         }; 
-        let docRef = db.collection('users').doc(context.auth.uid); 
-        docRef.set(user); 
+        const docRef = db.collection('users').doc(context.auth.uid); 
+        await docRef.set(user); 
     };  
 }
 
-let onUserDeleteFunction = function(data: any, context: any) {
+const onUserDeleteFunction = async function(data: any, context: any) {
     // get user 
-    let uid = context.auth.uid; 
+    const uid = context.auth.uid; 
     if (uid === null) {
         throw new functions.https.HttpsError('unauthenticated', 'No User to Suspend'); 
     } else {
-        let docRef = db.collection('users').doc(context.auth.uid); 
-        docRef.update({membership_status: entity.MembershipStatus.suspended}); 
+        const docRef = db.collection('users').doc(context.auth.uid); 
+        await docRef.update({membership_status: entity.MembershipStatus.suspended}); 
     }
 }
 
-export const routedUser  = userfunc; 
 export const userCreateAddInfo = userCreateAddInfoFunction
 export const onUserDelete = onUserDeleteFunction
