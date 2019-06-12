c2320d889143712f8406c4668c7442ee5a51cbc5
Raj Shrimali <rs@gatech.edu>
2019-06-12 19:34:25 -0400

    working deploymentgit add .

---
+++ public/precache-manifest.4c0b62069611d65f7408c5f063235ffc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "678d188e9f8356e250e9ff65f4fc3992",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ec9984b0520cc31a3c5",
+    "url": "/static/css/main.0565e67c.chunk.css"
+  },
+  {
+    "revision": "6000ecfcd7b7cdaedf2d",
+    "url": "/static/js/2.7ce91c26.chunk.js"
+  },
+  {
+    "revision": "6ec9984b0520cc31a3c5",
+    "url": "/static/js/main.600fc1e0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ public/precache-manifest.cc4ce29d0efcbbe6f361d9ce5c2b237f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7df01f075b030214beee91b7f346f68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5df0e0fbcea46f7b4bc",
+    "url": "/static/css/main.0565e67c.chunk.css"
+  },
+  {
+    "revision": "6000ecfcd7b7cdaedf2d",
+    "url": "/static/js/2.7ce91c26.chunk.js"
+  },
+  {
+    "revision": "c5df0e0fbcea46f7b4bc",
+    "url": "/static/js/main.5dd18f12.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
